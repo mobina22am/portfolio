7766b8e9798eff8072b14f85bffc91940eb7a656
Mobina Aghaeimaleki <120053168+mobina22am@users.noreply.github.com>
2024-08-10 13:14:53 +0100

    .

--- main.js
+++ main.js
@@ -1,7 +1,74 @@
+/*this is the mode change functionality*/
+let modeButton = document.getElementById('mode');
+let modeText = document.getElementById('modeText');
+let modeCircle = document.getElementById('modeCircle');
+let body = document.getElementById('body');
+let h = document.getElementById('toph1');
+let check = true;
+
+modeButton.addEventListener('click', modeChange);
+
+function modeChange(){
+    if (check){
+        console.log(modeButton)
+        modeButton.style.display = 'flex'
+        body.style.backgroundColor = 'rgb(254,235,195)'
+        document.documentElement.style.setProperty('--first', 'rgb(0,0,0)');
+
+        check = false
+    }
+
+    else{
+        modeButton.style.display = 'grid'
+        body.style.backgroundColor = 'black'
+
+        check = true
+    }
+}
+
+
+
+/*this is for the mouse follow*/
 let move = document.getElementById("move");
+
+
+document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    move.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    
+    }, {duration: 1000, fill: "forwards"})
+
+}
+
+/*these are the functionality of the menu button*/
 let menu = document.getElementById('menu');
-let navigation = document.getElementById('headerNavigation');
 let returnS = document.getElementById('return');
+let navigation = document.getElementById('headerNavigation');
+
+menu.addEventListener('click', menuClicked);
+returnS.addEventListener('click', returnClicked);
+
+
+function menuClicked(){
+    navigation.style.display = 'block';
+    menu.style.display = 'none';
+    returnS.style.display = 'block';
+}
+
+function returnClicked(){
+    navigation.style.display = 'none';
+    returnS.style.display = 'none';
+    menu.style.display = 'block';
+}
+
+
+
+
+
+/*these are the footer logos and their change of colour actions*/
 let link1 = document.getElementById('footerA1');
 let logo1 = document.getElementById('footerI1');
 
@@ -19,33 +86,6 @@ link2.addEventListener('mouseout', link2out);
 
 link3.addEventListener('mouseover', link3over);
 link3.addEventListener('mouseout', link3out);
-
-menu.addEventListener('click', menuClicked);
-returnS.addEventListener('click', returnClicked);
-
-
-document.body.onpointermove = event => {
-    const { clientX, clientY } = event;
-
-    move.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
-    
-    }, {duration: 1000, fill: "forwards"})
-
-}
-
-function menuClicked(){
-    navigation.style.display = 'block';
-    menu.style.display = 'none';
-    returnS.style.display = 'block';
-}
-
-function returnClicked(){
-    navigation.style.display = 'none';
-    returnS.style.display = 'none';
-    menu.style.display = 'block';
-}
 
 
 function link1over(){
